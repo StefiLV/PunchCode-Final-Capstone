eda83ea95fb9eccdaa47b43a43e2dc81350d8604
Brian Munoz <bryanmunoz@gmail.com>
2022-12-14 13:09:27 -0800

    fixed bridge tables

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -1,12 +1,13 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS user, event, hours, message, event_volunteer, admin CASCADE;
+DROP TABLE IF EXISTS user, event, hours, message, event_user CASCADE;
 
 CREATE TABLE user(
 	user_id SERIAL PRIMARY KEY,
 	first_name varchar (50),
 	last_name varchar(50),
-	username varchar (50) NOT NULL UNIQUE, --username is gonna be email!!!!!
+	org_name varchar(50),
+	username varchar (50), NOT NULL UNIQUE, --username is gonna be email!!!!!
 	birth_date date,
 	phone_number varchar (15),
 	address varchar (100),
@@ -16,7 +17,7 @@ CREATE TABLE user(
 	organization boolean,
 	verified boolean,
 	minor boolean,
-	role varchar(50) NOT NULL
+	role varchar(50) NOT NULL,
 );
 CREATE TABLE event (
 	event_id SERIAL PRIMARY KEY,
@@ -27,7 +28,7 @@ CREATE TABLE event (
 	start_time time NOT NULL,
 	end_time time NOT NULL,
 	description varchar (300),
-	volunteer_counter varchar (200), 
+	user_counter varchar (200), 
 );
 CREATE TABLE message (
 	message_id SERIAL PRIMARY KEY,
@@ -38,16 +39,18 @@ CREATE TABLE message (
 	attachment bytea(1gb),
 	archive boolean, --what data type?
 );
-CREATE TABLE event_volunteer (
-	volunteer_id int REFERENCES volunteer (volunteer_id),
+CREATE TABLE event_user(
+	user_id int REFERENCES user (user_id),
 	event_id int REFERENCES event (event_id),
-	approval_status boolean
+	approval_status boolean,
 );
 CREATE TABLE hours (
-	volunteer_id int REFERENCES volunteer (volunteer_id),
+	user_id int REFERENCES user (user_id),
 	event_id int REFERENCES event (event_id),
 	hours int,
 	approval_status boolean,
 );
 
 COMMIT;
+
+--ROLLBACK;
