dabc09ad492a03bcf3caff3c01f4910a2f264a14
pumpido <34642239+pumpido@users.noreply.github.com>
2022-12-18 12:47:11 -0800

    2022-12-18 no Messages update

---
+++ capstone/vue/src/views/NoMessages.vue
@@ -0,0 +1,155 @@
+<template>
+  <div id="no-msg">
+    
+    <div id="no-msg-head">
+
+      <div id="home-nav-bar">
+        <img src="../img/AppLogo.png" id="main-logo" alt="logo">
+
+        <div id="to-go-box">
+          <img src="../img/Message.png" id="hamburger" class="nav-icons">
+          <img src="../img/Bell.png" id="hamburger" class="nav-icons">
+          <img src="../img/Hamburger.png" id="hamburger" class="nav-icons">
+        </div>
+      
+      </div>
+
+    </div>
+
+    <div id="no-msg-body">
+      
+      <div id="msg-title">
+        <h2 id="msg-h2">MESSAGES</h2>
+      </div>
+
+      <div id="msg-envLogo">
+        <img src="../img/envLogo.png">
+      </div>
+
+      <div id="noMsgs">
+        <h2 id="no-msg">There are no messages!</h2>
+      </div>
+
+      <div id="msg-body2">
+        <div>
+          <input id="msg-input" type="text">
+          <button id="msg-btn">SEND</button>
+        </div>
+      </div>
+
+    </div>
+  
+
+    <div id="no-msg-footer"> 
+      COPYRIGHT © 2022 PUNCHCODE COHORT 3
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "message"
+};
+</script>
+
+<style scoped>
+
+#no-msg{
+  overflow: no-scroll;
+}
+
+#no-msg-head {
+  height: 15vh;
+}
+
+#no-msg-body {
+  height: 80vh;
+  display: grid;
+  grid-template-areas:
+  msg-title
+  msg-envLogo
+  noMsgs
+  msg-body2
+  no-msg-footer
+}
+
+#no-msg-footer {
+  border-top: 2px solid black;
+  height: 5vh;
+  padding-top: 20px;
+  text-align: center;
+}
+
+#home-nav-bar {
+  max-height: 15vh;
+  display: flex;
+}
+
+#to-go-box {
+  float: right;
+  padding-top: 15px;
+  padding-right: 10px;
+}
+
+.nav-icons {
+  width: 6vw;
+  height: 3.5vh;
+  margin-left: 12px;
+}
+
+#main-logo {
+  display: block;
+  margin-right: auto;
+  width: 15%;
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+
+.msg-ts, .msg {
+  margin-left: 6px;
+}
+
+#msg-title {
+  height: 15%;
+  text-align: center;
+  letter-spacing: 3px;
+}
+
+#noMsgs{
+ display: grid;
+  height: 15%;
+  text-align: center;
+  align-items: end;
+  color: #0964cb;
+
+}
+
+#msg-envLogo{ 
+  display: grid;
+  justify-content: center;
+}
+
+#msg-body2{
+  height: 5vh;
+  display: grid;
+  align-items: end;
+  justify-items: center;
+
+}
+
+#msg-input {
+  width: 60vw;
+  height: 50px;
+
+}
+
+#msg-btn {
+  width: 30vw;
+  height: 56px;
+  background: #0663CB;
+  color: white;
+  letter-spacing: 2px;
+}
+
+</style>
