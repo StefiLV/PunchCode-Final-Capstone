587d90a41010f02be1496cf5fb7359aab47ab349
Brian Munoz <bryanmunoz@gmail.com>
2022-12-18 11:27:27 -0800

    changed bday to varchar

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -1,14 +1,14 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS user, event, hours, message, event_user, cause CASCADE;
+DROP TABLE IF EXISTS event_user, users, event, hours, message, cause CASCADE;
 
-CREATE TABLE user(
+CREATE TABLE users (
 	user_id SERIAL PRIMARY KEY,
 	name varchar(100),
-	username varchar (50), NOT NULL UNIQUE, --username is gonna be email!!!!!
-	birth_date date,
+	username varchar (50) NOT NULL UNIQUE,
+	birth_date varchar(10),
 	phone_number varchar (15),
-	description varchar(Max),
+	description varchar(500),
 	address varchar (100),
 	profile_pic varchar(300),
 	hero_banner varchar(300),
@@ -16,7 +16,7 @@ CREATE TABLE user(
 	organization boolean,
 	verified boolean,
 	minor boolean,
-	role varchar(50) NOT NULL,
+	role varchar(50) NOT NULL
 );
 CREATE TABLE event (
 	id SERIAL PRIMARY KEY,
@@ -26,37 +26,38 @@ CREATE TABLE event (
 	end_date date NOT NULL,
 	start_time time NOT NULL,
 	end_time time NOT NULL,
-	description varchar(Max),
-	user_counter varchar (200), 
+	description varchar(500),
+	user_counter varchar (200)
 );
 CREATE TABLE message (
 	id SERIAL PRIMARY KEY,
-	sender_id SERIAL NOT NULL,
-	receiver_id SERIAL NOT NULL,
-	time_stamp smalldatetime NOT NULL,
+	sender_id int REFERENCES users(user_id) NOT NULL,
+	receiver_id int REFERENCES users(user_id) NOT NULL,
+	time_stamp TIMESTAMP NOT NULL,
 	message_text varchar (300) NOT NULL,
 	attachment varchar(300),
-	archive boolean, --what data type?
+	archive boolean
 );
 CREATE TABLE event_user(
 	id SERIAL PRIMARY KEY,
-	user_id int REFERENCES user (user_id),
-	id int REFERENCES event (id),
-	approval_status varchar(10),
+	user_id int REFERENCES users (user_id),
+	event_id int REFERENCES event (id),
+	approval_status varchar(10)
 );
 CREATE TABLE hours (
 	id SERIAL PRIMARY KEY,
-	user_id int REFERENCES user (user_id),
-	id int REFERENCES event (id),
+	user_id int REFERENCES users (user_id),
+	event_id int REFERENCES event (id),
 	hours int,
-	approval_status varchar(10),
+	approval_status varchar(10)
 );
 CREATE TABLE cause(
 	id SERIAL PRIMARY KEY,
-	id int REFERENCES event (id),
-	description varchar(50),
+	event_id int REFERENCES event (id),
+	description varchar(50)
 );
 
 COMMIT;
 
 --ROLLBACK;
+
