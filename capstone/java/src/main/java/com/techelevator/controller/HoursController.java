74ac3d9834d18ec73de3e7d915c1639959332f41
Kaleb Mccalla <kalebmccalla@gmail.com>
2022-12-20 11:36:37 -0800

    updated daos dec 20

--- capstone/java/src/main/java/com/techelevator/controller/HoursController.java
+++ capstone/java/src/main/java/com/techelevator/controller/HoursController.java
@@ -21,7 +21,7 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api")
 
 public class HoursController {
 
