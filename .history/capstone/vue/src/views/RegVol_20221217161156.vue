769141a18b3baafa8572743f26ff0d060e4fd99c
Chris Gomez <chriseloy98@gmail.com>
2022-12-19 20:40:03 -0800

    quick update

---
+++ .history/capstone/vue/src/views/RegVol_20221217161156.vue
@@ -0,0 +1,237 @@
+<template>
+  <div id="regOrg" class="text-center">
+    <div id="regOrgNav">
+      <div id="to-go-box" v-on:click="openBurger">
+        <img src="../img/Hamburger.png" id="hamburger" />
+      </div>
+    </div>
+
+    <div id="regOrgLogo">
+      <img id="main-logo" src="../img/AppLogo.png" alt="" />
+      <h2>Volunteer Sign Up</h2>
+    </div>
+
+    <div id="regOrgBody">
+      <div id="regOrgForm">
+        <form class="form-register" @submit.prevent="register">
+          <input
+            type="name"
+            id="name"
+            class="form-control"
+            placeholder="NAME"
+            v-model="user.name"
+            required
+            autofocus
+          /><br />
+          <input
+            type="text"
+            id="username"
+            class="form-control"
+            placeholder="EMAIL"
+            v-model="user.username"
+            required
+          /><br />
+          <input
+            type="password"
+            id="password"
+            class="form-control"
+            placeholder="PASSWORD"
+            v-model="user.password"
+            required
+          /><br />
+          <input
+            type="password"
+            id="confirmPassword"
+            class="form-control"
+            placeholder="CONFIRM PASSWORD"
+            v-model="user.confirmPassword"
+            required
+          /><br />
+          <input
+            type="address"
+            id="address"
+            class="form-control"
+            placeholder="ADDRESS"
+            v-model="user.address"
+            required
+          /><br />
+          <input
+            type="city"
+            id="city"
+            class="form-control"
+            placeholder="CITY"
+            v-model="user.city"
+            required
+          /><br />
+          <input
+            type="state"
+            id="state"
+            class="form-control"
+            placeholder="STATE"
+            v-model="user.state"
+            required
+          /><br />
+          <input
+            type="zipCode"
+            id="zipCode"
+            class="form-control"
+            placeholder="ZIP CODE"
+            v-model="user.zipCode"
+            required
+          /><br />
+          <input
+            type="text"
+            onfocus="(this.type='date')"
+            id="dateBirth"
+            class="form-control"
+            placeholder="DATE OF BIRTH"
+            v-model="user.dateBirth"
+            required
+          /><br />
+          <button
+            id="regOrgBtn"
+            class="btn btn-lg btn-primary btn-block"
+            type="submit"
+          >
+            SUBMIT
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import authService from "../services/AuthService";
+
+export default {
+  name: "regVol",
+  data() {
+    return {
+      user: {
+        username: "",
+        password: "",
+        confirmPassword: "",
+        role: "user",
+      },
+      registrationErrors: false,
+      registrationErrorMsg:
+        "There were problems registering this user.",
+    };
+  },
+  methods: {
+    register() {
+      if (this.user.password != this.user.confirmPassword) {
+        this.registrationErrors = true;
+        this.registrationErrorMsg =
+          "Password & Confirm Password do not match.";
+      } else {
+        authService
+          .register(this.user)
+          .then((response) => {
+            if (response.status == 201) {
+              this.$router.push({
+                path: "/login",
+                query: { registration: "success" },
+              });
+            }
+          })
+          .catch((error) => {
+            const response = error.response;
+            this.registrationErrors = true;
+            if (response.status === 400) {
+              this.registrationErrorMsg =
+                "Bad Request: Validation Errors";
+            }
+          });
+      }
+    },
+    clearErrors() {
+      this.registrationErrors = false;
+      this.registrationErrorMsg =
+        "There were problems registering this user.";
+    },
+  },
+};
+</script>
+
+<style scoped>
+#regOrgNav {
+  height: 5vh;
+}
+
+#regOrgLogo {
+  height: 25vh;
+}
+
+#main-logo {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 265px;
+}
+
+h2 {
+  text-align: center;
+  text-decoration: underline;
+}
+
+#regOrgBody {
+  height: 70vh;
+  background-image: url("../img/RegBG.png");
+  background-repeat: no-repeat;
+  background-position: 43% 40%;
+}
+
+#regOrgForm {
+  height: 300px;
+  text-align: center;
+  margin: auto;
+  padding-top: 5vh;
+}
+
+.form-control {
+  width: 65vw;
+  height: 35px;
+  margin-bottom: 25px;
+  border-radius: 5px;
+  border: 1px solid white;
+}
+
+.form-control:focus {
+  font-size: 20px;
+}
+
+.form-control::placeholder {
+  color: black;
+  letter-spacing: 4px;
+  padding-left: 10px;
+}
+
+#regOrgBtn {
+  background: #0045b8;
+  border: 1px solid #0045b8;
+  color: white;
+  width: 35vw;
+  height: 45px;
+  border-radius: 5px;
+  margin-bottom: 25px;
+}
+
+#to-go-box {
+  float: right;
+  margin-right: 10px;
+}
+
+.hamburger {
+  width: 35px;
+  height: 5px;
+  background-color: black;
+  margin: 6px 0;
+}
+#hamburger {
+  width: 6%;
+  float: right;
+  margin-top: 5px;
+}
+</style>
---
+++ .history/capstone/vue/src/views/VolProfile_20221218184753.vue
@@ -0,0 +1,182 @@
+<template>
+  <div id="profile">
+    <div id="profile-head">
+      <div id="home-nav-bar">
+        <img src="" alt="" />
+
+        <img src="../img/AppLogo.png" id="main-logo" alt="logo" />
+
+        <div id="to-go-box" v-on:click="openBurger">
+          <img
+            src="../img/Message.png"
+            id="message"
+            class="nav-icons"
+          />
+          <img src="../img/Bell.png" id="bell" class="nav-icons" />
+          <img
+            src="../img/Hamburger.png"
+            id="hamburger"
+            class="nav-icons"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div id="profile-title">
+      MY PROFILE
+    </div>
+
+    <div id="personal-info">
+      <div class="event-box">
+        <img class="event-box-logo" src="../img/ProfilePic.png" />
+        <h3>NAME</h3>
+        <h4>EMAIL<br />PHONE NUMBER<br />ADDRESS</h4>
+        <!-- <button class="expand-btn">See More</button> -->
+      </div>
+
+      <div class="event-bio">
+        <p>ADD BIO (Optional)</p>
+        <textarea
+          id="textarea-bio"
+          type="text"
+          placeholder="Bio (Optional)"
+          name="name"
+        />
+
+        <h3></h3>
+        <h4></h4>
+      </div>
+
+      <div class="event-box">
+        <h3>MY HOURS</h3>
+        <h4>
+          Three Square<br />4190 N Pecos Rd<br />Nov. 1, 2022 - Dec.
+          23, 2022
+        </h4>
+      </div>
+    </div>
+
+    <div id="main-footer">
+      COPYRIGHT © 2022 PUNCHCODE COHORT 3
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "volProfile",
+};
+</script>
+
+<style scoped>
+#home {
+  overflow: no-scroll;
+}
+
+#home-nav-bar {
+  max-height: 15vh;
+  display: flex;
+}
+
+#to-go-box {
+  float: right;
+  display: flex;
+  width: 110px;
+  justify-content: space-between;
+  align-items: center;
+  padding-right: 10px;
+}
+
+.nav-icons {
+  width: 6vw;
+  height: 3.5vh;
+}
+
+.hamburger {
+  width: 35px;
+  height: 5px;
+  background-color: black;
+  margin: 6px 0;
+}
+
+#profile-title {
+  text-align: center;
+}
+
+#personal-info {
+  height: 85vh;
+  overflow: scroll;
+  width: 90vw;
+  margin: auto;
+  padding-top: 15px;
+}
+
+img {
+  max-width: 100%;
+  height: 50%;
+}
+.event-bio {
+  text-align: center;
+  width: 80vw;
+  height: 15vh;
+  border: 2px solid black;
+  padding-left: 15px;
+  border-radius: 10px;
+  margin-left: 15px;
+  margin-bottom: 15px;
+}
+
+.event-box {
+  width: 80vw;
+  height: 20vh;
+  border: 2px solid black;
+  text-align: left;
+  padding-left: 15px;
+  border-radius: 10px;
+  margin-left: 15px;
+  margin-bottom: 15px;
+}
+
+.expanded-box {
+  height: 30vh;
+}
+
+#main-logo {
+  display: block;
+  margin-right: auto;
+  width: 15%;
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+
+.event-box-logo {
+  width: 160px;
+  float: right;
+  position: relative;
+  top: 20px;
+  right: 10px;
+}
+
+.innovate-for-vegas-logo {
+  width: 160px;
+  float: right;
+  position: relative;
+  bottom: 15px;
+  right: 15px;
+}
+
+#main-footer {
+  border-top: 2px solid black;
+  height: 5vh;
+  padding-top: 5px;
+  text-align: center;
+}
+h3 {
+  text-decoration: underline;
+}
+
+#textarea-bio {
+  width: 85%;
+  height: 40%;
+}
+</style>
