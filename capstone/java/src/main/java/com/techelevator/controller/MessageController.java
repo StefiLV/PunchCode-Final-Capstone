295878de4f03a067b0d39d38a5e59ff1711cd1e0
Kaleb Mccalla <kalebmccalla@gmail.com>
2022-12-22 15:27:04 -0800

    ignore

--- capstone/java/src/main/java/com/techelevator/controller/MessageController.java
+++ capstone/java/src/main/java/com/techelevator/controller/MessageController.java
@@ -1,47 +1,47 @@
-//package com.techelevator.controller;
-//
-//import com.techelevator.model.Message;
-//import java.util.List;
-//import com.techelevator.dao.MessageDao;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.web.server.ResponseStatusException;
-//
-//import javax.validation.Valid;
-//
-//@RestController
-//@RequestMapping("/api")
-//public class MessageController {
-//
-//    private final MessageDao messageDao;
-//
-//    public MessageController(MessageDao messageDao){ this.messageDao = messageDao; }
-//
-//    @RequestMapping(method = RequestMethod.GET)
-//    public List<Message> list(){ return messageDao.list(); }
-//
-//    @RequestMapping(path = "/messages/{id}", method = RequestMethod.GET)
-//    public Message getById(@PathVariable int msgId){
-//        Message message = messageDao.getById(msgId);
-//        if(message == null){
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Message not found. ");
-//        }else {
-//            return messageDao.getById(msgId);
-//        }
-//    }
-//    @ResponseStatus(HttpStatus.CREATED)
-//    @RequestMapping( method = RequestMethod.POST)
-//    public void create(@Valid @RequestBody Message message){
-//        messageDao.createMsg(message);
-//    }
-//
-//    @RequestMapping(path = "/{id}", method = RequestMethod.PUT)
-//    public void update(@Valid @RequestBody Message message, @Valid @PathVariable int msgId) {
-//        messageDao.updateMsg(message, msgId);
-//    }
-//    @ResponseStatus(HttpStatus.NO_CONTENT)
-//    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
-//    public void delete(@PathVariable int msgId){
-//        messageDao.deleteMsg(msgId);
-//    }
-//}
+package com.techelevator.controller;
+
+import com.techelevator.model.Message;
+import java.util.List;
+import com.techelevator.dao.MessageDao;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class MessageController {
+
+    private final MessageDao messageDao;
+
+    public MessageController(MessageDao messageDao){ this.messageDao = messageDao; }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Message> list(){ return messageDao.list(); }
+
+    @RequestMapping(path = "/messages/{id}", method = RequestMethod.GET)
+    public Message getById(@PathVariable int msgId){
+        Message message = messageDao.getById(msgId);
+        if(message == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Message not found. ");
+        }else {
+            return messageDao.getById(msgId);
+        }
+    }
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "/messages", method = RequestMethod.POST)
+    public void create(@Valid @RequestBody Message message){
+        messageDao.createMsg(message);
+    }
+
+    @RequestMapping(path = "/messages/{id}", method = RequestMethod.PUT)
+    public void update(@Valid @RequestBody Message message, @Valid @PathVariable int msgId) {
+        messageDao.updateMsg(message, msgId);
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RequestMapping(path = "/messages/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable int msgId){
+        messageDao.deleteMsg(msgId);
+    }
+}
--- capstone/java/src/main/java/com/techelevator/model/Message.java
+++ capstone/java/src/main/java/com/techelevator/model/Message.java
@@ -1,47 +1,47 @@
-//package com.techelevator.model;
-//
-//public class Message {
-//    private int msgId;
-//    private int senderId;
-//    private int receiverId;
-//    private String timeStamp;
-//    private String text; //message_text from database
-//    private String attachment;
-//    private boolean archive;
-//
-//
-//    public Message(){ }
-//
-//    public Message(int msgId, int senderId, int receiverId, String timeStamp, String text, String attachment, boolean archive){
-//        this.msgId = msgId;
-//        this.senderId = senderId;
-//        this.receiverId = receiverId;
-//        this.timeStamp = timeStamp;
-//        this.text = text;
-//        this.attachment = attachment;
-//        this.archive = archive;
-//    }
-//
-//    public int getMsgId(){ return msgId; }
-//    public void setMsgId(int msgId){ this.msgId = msgId; }
-//
-//    public int getSenderId(){ return senderId; }
-//    public void setSenderId(int senderId){ this.senderId = senderId; }
-//
-//    public int getReceiverId(){ return receiverId; }
-//    public void setReceiverId(int receiverId){ this.receiverId = receiverId; }
-//
-//    public String getTimeStamp(){ return timeStamp; }
-//    public void setTimeStamp(String timeStamp){ this.timeStamp = timeStamp; }
-//
-//    public String getText(){ return text; }
-//    public void setText(String text){ this.text = text; }
-//
-//    public String getAttachment(){ return attachment; }
-//    public void setAttachment(String attachment){ this.attachment = attachment; }
-//
-//    public boolean isArchive(){ return archive; }
-//    public void setArchive(boolean archive){ this.archive = archive; }
-//
-//
-// }
+package com.techelevator.model;
+
+public class Message {
+    private int msgId;
+    private int senderId;
+    private int receiverId;
+    private String time_stamp;
+    private String message_text; //message_message_text from database
+    private String attachment;
+    private boolean archive;
+
+
+    public Message(){ }
+
+    public Message(int msgId, int senderId, int receiverId, String time_stamp, String message_text, String attachment, boolean archive){
+        this.msgId = msgId;
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.time_stamp = time_stamp;
+        this.message_text = message_text;
+        this.attachment = attachment;
+        this.archive = archive;
+    }
+
+    public int getMsgId(){ return msgId; }
+    public void setMsgId(int msgId){ this.msgId = msgId; }
+
+    public int getSenderId(){ return senderId; }
+    public void setSenderId(int senderId){ this.senderId = senderId; }
+
+    public int getReceiverId(){ return receiverId; }
+    public void setReceiverId(int receiverId){ this.receiverId = receiverId; }
+
+    public String getTimeStamp(){ return time_stamp; }
+    public void setTimeStamp(String timeStamp){ this.time_stamp = timeStamp; }
+
+    public String getMessage_text(){ return message_text; }
+    public void setMessage_text(String message_text){ this.message_text = message_text; }
+
+    public String getAttachment(){ return attachment; }
+    public void setAttachment(String attachment){ this.attachment = attachment; }
+
+    public boolean isArchive(){ return archive; }
+    public void setArchive(boolean archive){ this.archive = archive; }
+
+
+ }
